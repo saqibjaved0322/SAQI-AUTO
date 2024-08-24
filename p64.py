6c2e2fca9ba5fba7d3a382798bcb9607218401d6
saqibjaved0322 <105146668+saqibjaved0322@users.noreply.github.com>
2024-08-25 02:16:29 +0500

    Create p64.py

---
+++ p64.py
@@ -0,0 +1,10 @@
+import os,platform
+os.system('git pull')
+os.system("clear")
+print('\033[92;1m Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/LDKdovG09Ob2Ai0prMXtCG')')
+fbd=platform.architecture()[0]
+if fbd=="32bit":
+    __import__("p64")
+elif fbd=="64bit":
+    __import__("p64")
