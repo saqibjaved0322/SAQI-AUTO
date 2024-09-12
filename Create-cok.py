03ca79dadbad1b175e040e7a2bac9dd5ef0e252a
saqibjaved0322 <105146668+saqibjaved0322@users.noreply.github.com>
2024-09-12 11:19:48 +0500

    Update Create-cok.py

--- Create-cok.py
+++ Create-cok.py
@@ -1,20 +1,11 @@
-import os
-import platform
-
+import os,platform
 os.system('git pull --quiet 2>/dev/null')
 os.system("clear")
 print('\033[92;1m [\033[97;1m•\033[92;1m] Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/DsCVNjYtO5027qagbKaNbJ')
-
-saqi = platform.architecture()[0]
-
-if saqi == "32bit":
-    os.system("clear")
-    exit("\033[91;1m 32Bit Device Not Supported")
-
-elif saqi == "64bit":
-    try:
-        __import__("cook2")
-    except ImportError as e:
-        print(f"\033[91;1m ImportError: {e}")
-        print("\033[91;1m Ensure that 'cook2' is compiled for 64-bit architecture.")
+saqi=platform.architecture()[0]
+if saqi=="32bit":
+    os.system("clear");exit("\033[91;1m 32Bit Device Not Supported")
+    __import__("cook2")
+elif saqi=="64bit":
+    __import__("cook2")
